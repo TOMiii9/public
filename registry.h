83b9feb335f53e32f96d5a36663c5ea1a467a5da
TOMiii9 <tamaskrizsanyik@gmail.com>
2019-01-26 12:40:14 +0000

    It's thread safe now, can remove comments etc.

--- registry.h
+++ registry.h
@@ -28,88 +28,89 @@ For more information, please refer to <http://unlicense.org>
 #ifndef REGISTRY_H_
 #define REGISTRY_H_
 
-#include <map>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <sstream>
 #include <Windows.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <string>
 #include <vector>
 
-bool reg_load(const char *file_name);
-void reg_save(const char *file_name);
+bool RegLoad(const char *file_name);
+void RegSave(const char *file_name);
 
-void reg_set_string(const char *name, const char *value);
-void reg_set_int32(const char *name, int value);
-void reg_set_float32(const char *name, float value);
-void reg_set_float64(const char *name, double value);
+void RegSetString(const char *name, const char *value);
+void RegSetInt32(const char *name, int value);
+void RegSetFloat32(const char *name, float value);
+void RegSetFloat64(const char *name, double value);
 
-std::string reg_get_string(const char *name, const char* default_value);
-int reg_get_int32(const char *name, int default_value);
-float reg_get_float32(const char *name, float default_value);
-double reg_get_float64(const char *name, double default_value);
+std::string RegGetString(const char *name, const char *default_value);
+int         RegGetInt32(const char *name, int default_value);
+float       RegGetFloat32(const char *name, float default_value);
+double      RegGetFloat64(const char *name, double default_value);
 
 
 #ifdef REGISTRY_IMPLEMENTATION
 
-namespace
-{
-    std::map<std::string, std::string> kv_pairs;
+namespace {
+std::map<std::string, std::string> kv_pairs;
+std::mutex                         registry_mutex;
 
-    void string_trim(std::string& s, const char* t = " \t\n\r\f\v")
-    {
-        s.erase(0, s.find_first_not_of(t));
-        s.erase(s.find_last_not_of(t) + 1);
-    }
-
-    void string_split(const std::string &s, char delim, std::vector<std::string> &result) {
-        std::stringstream ss(s);
-        std::string item;
-        while (std::getline(ss, item, delim)) 
-        {
-            if (item.length())
-            {
-                result.push_back(item);
-            }
-        }
-    }
-
-    inline bool file_exists(const char *name) 
-    {
-        std::ifstream f(name);
-        return f.good();
-    }
-
-    void sys_error(const char *error)
-    {
-        MessageBoxA(0, error, "Error", MB_OK);
-        abort();
-    }
-
+void TrimString(std::string &s, const char *t = " \t\n\r\f\v") {
+    s.erase(0, s.find_first_not_of(t));
+    s.erase(s.find_last_not_of(t) + 1);
 }
 
-bool reg_load(const char *file_name)
-{
-    if (file_exists(file_name))
-    {
+void SplitString(const std::string &s, char delim, std::vector<std::string> &result) {
+    std::stringstream ss(s);
+    std::string       item;
+    while (std::getline(ss, item, delim)) {
+        if (item.length()) {
+            result.push_back(item);
+        }
+    }
+}
+
+inline bool FileExists(const char *name) {
+    std::ifstream f(name);
+    return f.good();
+}
+
+void sys_error(const char *error) {
+    MessageBoxA(0, error, "Error", MB_OK);
+    abort();
+}
+
+void RemoveComments(std::string &line) {
+    auto pos = line.find_last_of(';');
+    if (pos != std::string::npos) {
+        line.erase(pos, line.length() - pos);
+    }
+}
+
+} // namespace
+
+bool RegLoad(const char *file_name) {
+    std::scoped_lock registry_lock(registry_mutex);
+
+    if (FileExists(file_name)) {
         std::ifstream in("config.txt");
-        std::string str;
-        
+        std::string   line;
+
         kv_pairs.clear();
 
-        while (std::getline(in, str)) 
-        {
+        while (std::getline(in, line)) {
             std::vector<std::string> tokens;
 
-            string_trim(str);
-            string_split(str, ' ', tokens);
+            RemoveComments(line);
+            TrimString(line);
+            SplitString(line, ' ', tokens);
 
-            if (tokens.size() > 1)
-            {
+            if (tokens.size() > 1) {
                 std::string value;
 
-                for (int i = 1; i < tokens.size(); i++)
-                {
+                for (int i = 1; i < tokens.size(); i++) {
                     value += tokens[i];
                 }
 
@@ -123,74 +124,78 @@ bool reg_load(const char *file_name)
     return false;
 }
 
-void reg_save(const char *file_name)
-{	
-    for (auto &pair : kv_pairs)
-    {
+void RegSave(const char *file_name) {
+    std::scoped_lock registry_lock(registry_mutex);
+
+    for (auto &pair : kv_pairs) {
         std::ofstream f;
 
-        f.open("config.txt");
+        f.open(file_name);
         f << pair.first << " " << pair.second << "\n";
     }
 }
 
-void reg_set_string(const char *name, const char *value)
-{
+void RegSetString(const char *name, const char *value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     kv_pairs[name] = value;
 }
 
-void reg_set_int32(const char *name, int value)
-{
+void RegSetInt32(const char *name, int value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     kv_pairs[name] = std::to_string(value);
 }
 
-void reg_set_float32(const char *name, float value)
-{
+void RegSetFloat32(const char *name, float value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     kv_pairs[name] = std::to_string(value);
 }
 
-void reg_set_float64(const char *name, double value)
-{
+void RegSetFloat64(const char *name, double value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     kv_pairs[name] = std::to_string(value);
 }
 
-std::string reg_get_string(const char *name, const char* default_value)
-{
+std::string RegGetString(const char *name, const char *default_value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end())
-    {        
+    if (value != kv_pairs.end()) {
         return value->second;
     }
     return default_value;
 }
 
-int reg_get_int32(const char *name, int default_value)
-{
+int RegGetInt32(const char *name, int default_value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end())
-    {
+    if (value != kv_pairs.end()) {
         int ret = atoi(value->second.c_str());
         return ret;
     }
     return default_value;
 }
 
-float reg_get_float32(const char *name, float default_value)
-{
+float RegGetFloat32(const char *name, float default_value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end())
-    {
+    if (value != kv_pairs.end()) {
         float ret = std::stof(value->second.c_str());
         return ret;
     }
     return default_value;
 }
 
-double reg_get_float64(const char *name, double default_value)
-{
+double RegGetFloat64(const char *name, double default_value) {
+    std::scoped_lock registry_lock(registry_mutex);
+
     auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end())
-    {
+    if (value != kv_pairs.end()) {
         double ret = std::stod(value->second.c_str());
         return ret;
     }
