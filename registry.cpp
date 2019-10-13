a8a47dadca1e6f280455f1c0bd1bc22ba8b8969e
TOMiii9 <tamaskrizsanyik@gmail.com>
2019-10-13 08:57:29 +0100

    registry

---
+++ registry.cpp
@@ -0,0 +1,184 @@
+#include "registry.h"
+
+#include <Windows.h>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <shared_mutex>
+#include <sstream>
+#include <vector>
+#include "tokenizer.h"
+#include "util.h"
+
+namespace {
+std::map<std::string, std::string> kv_pairs;
+std::shared_mutex                  registry_mutex;
+} // namespace
+
+static void parse_block(nano_lexer &lexer) {
+    std::string current_build;
+    std::string block_type;
+
+#ifdef _DEBUG
+    current_build = "debug";
+#else
+    current_build = "release";
+#endif
+
+    nano_lexer::token token;
+    if (lexer.expect_token(token, nano_lexer::Token_String)) {
+        block_type = token.text;
+        if (lexer.expect_token(token, nano_lexer::Token_Open_Curly_Brace)) {
+            while (lexer.state.ok) {
+                token = lexer.get_token();
+                switch (token.type) {
+                    case nano_lexer::Token_Identifier: {
+                        std::string var_name = token.text;
+                        if (lexer.expect_token(token, nano_lexer::Token_Equal)) {
+                            if (lexer.expect_token(token, nano_lexer::Token_String)) {
+                                std::string var_value = token.text;
+                                // report("cfg_token: %s -> %s\n", var_name.c_str(), var_value.c_str());
+                                kv_pairs[var_name] = var_value;
+                            } else {
+                                lexer.error("parse_block: missing value after '='");
+                            }
+                        } else {
+                            lexer.error("parse_block: missing '='");
+                        }
+                    } break;
+
+                    case nano_lexer::Token_Close_Curly_Brace:
+                        // finished with this block
+                        return;
+
+                    default:
+                        lexer.error("parse_block: unexpected token");
+                }
+            }
+        } else {
+            lexer.error("opening curly brace is missing");
+        }
+    } else {
+        lexer.error("config 'platform' expected");
+    }
+}
+
+bool reg_load(const char *file_name) {
+    std::unique_lock registry_lock(registry_mutex);
+    nano_lexer       lexer;
+
+    lexer.from_file(file_name);
+    while (lexer.state.ok) {
+        nano_lexer::token token = lexer.get_token();
+
+        if (token.type == nano_lexer::Token_End) {
+            return true;
+        }
+
+        if (token.type == nano_lexer::Token_Identifier && token.text == "config") {
+            parse_block(lexer);
+        }
+    }
+
+    return false;
+}
+
+void reg_save(const char *file_name) {
+    std::unique_lock registry_lock(registry_mutex);
+
+    for (auto &pair : kv_pairs) {
+        std::ofstream f;
+
+        f.open(file_name);
+        f << pair.first << " " << pair.second << "\n";
+    }
+}
+
+void reg_set_string(const char *name, const char *value) {
+    std::unique_lock registry_lock(registry_mutex);
+
+    kv_pairs[name] = value;
+}
+
+void reg_set_i32(const char *name, int value) {
+    std::unique_lock registry_lock(registry_mutex);
+
+    kv_pairs[name] = std::to_string(value);
+}
+
+void reg_set_f32(const char *name, float value) {
+    std::unique_lock registry_lock(registry_mutex);
+
+    kv_pairs[name] = std::to_string(value);
+}
+
+void reg_set_f64(const char *name, double value) {
+    std::unique_lock registry_lock(registry_mutex);
+
+    kv_pairs[name] = std::to_string(value);
+}
+
+std::string reg_get_string(const char *name, const char *default_value) {
+    std::shared_lock registry_lock(registry_mutex);
+
+    auto value = kv_pairs.find(name);
+    if (value != kv_pairs.end()) {
+        return value->second;
+    }
+    return default_value;
+}
+
+int reg_get_i32(const char *name, int default_value) {
+    std::shared_lock registry_lock(registry_mutex);
+
+    auto value = kv_pairs.find(name);
+    if (value != kv_pairs.end()) {
+        int ret = atoi(value->second.c_str());
+        return ret;
+    }
+    return default_value;
+}
+
+float reg_get_f32(const char *name, float default_value) {
+    std::shared_lock registry_lock(registry_mutex);
+
+    auto value = kv_pairs.find(name);
+    if (value != kv_pairs.end()) {
+        float ret = std::stof(value->second.c_str());
+        return ret;
+    }
+    return default_value;
+}
+
+std::array<float, 3> reg_get_3f32(const char *name) {
+    std::shared_lock registry_lock(registry_mutex);
+
+    std::array<float, 3> ret{0.0f, 0.0f, 0.0f};
+    auto                 value = kv_pairs.find(name);
+
+    if (value != kv_pairs.end()) {
+        string_array out;
+        split_string(value->second, &out, ' ');
+
+        if (out.size() == 3) {
+            ret[0] = float(atof(out[0].c_str()));
+            ret[1] = float(atof(out[1].c_str()));
+            ret[2] = float(atof(out[2].c_str()));
+        }
+
+        return ret;
+    }
+    return ret;
+}
+
+double reg_get_f64(const char *name, double default_value) {
+    std::shared_lock registry_lock(registry_mutex);
+
+    auto value = kv_pairs.find(name);
+    if (value != kv_pairs.end()) {
+        double ret = std::stod(value->second.c_str());
+        return ret;
+    }
+    return default_value;
+}
--- registry.h
+++ registry.h
@@ -28,180 +28,20 @@ For more information, please refer to <http://unlicense.org>
 #ifndef REGISTRY_H_
 #define REGISTRY_H_
 
-#include <Windows.h>
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <mutex>
-#include <sstream>
 #include <string>
-#include <vector>
+#include <array>
 
-bool RegLoad(const char *file_name);
-void RegSave(const char *file_name);
+bool reg_load(const char *file_name);
+void reg_save(const char *file_name);
 
-void RegSetString(const char *name, const char *value);
-void RegSetInt32(const char *name, int value);
-void RegSetFloat32(const char *name, float value);
-void RegSetFloat64(const char *name, double value);
+void reg_set_string(const char *name, const char *value);
+void reg_set_i32(const char *name, int value);
+void reg_set_f32(const char *name, float value);
+void reg_set_f64(const char *name, double value);
 
-std::string RegGetString(const char *name, const char *default_value);
-int         RegGetInt32(const char *name, int default_value);
-float       RegGetFloat32(const char *name, float default_value);
-double      RegGetFloat64(const char *name, double default_value);
-
-
-#ifdef REGISTRY_IMPLEMENTATION
-
-namespace {
-std::map<std::string, std::string> kv_pairs;
-std::mutex                         registry_mutex;
-
-void TrimString(std::string &s, const char *t = " \t\n\r\f\v") {
-    s.erase(0, s.find_first_not_of(t));
-    s.erase(s.find_last_not_of(t) + 1);
-}
-
-void SplitString(const std::string &s, char delim, std::vector<std::string> &result) {
-    std::stringstream ss(s);
-    std::string       item;
-    while (std::getline(ss, item, delim)) {
-        if (item.length()) {
-            result.push_back(item);
-        }
-    }
-}
-
-inline bool FileExists(const char *name) {
-    std::ifstream f(name);
-    return f.good();
-}
-
-void sys_error(const char *error) {
-    MessageBoxA(0, error, "Error", MB_OK);
-    abort();
-}
-
-void RemoveComments(std::string &line) {
-    auto pos = line.find_last_of(';');
-    if (pos != std::string::npos) {
-        line.erase(pos, line.length() - pos);
-    }
-}
-
-} // namespace
-
-bool RegLoad(const char *file_name) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    if (FileExists(file_name)) {
-        std::ifstream in("config.txt");
-        std::string   line;
-
-        kv_pairs.clear();
-
-        while (std::getline(in, line)) {
-            std::vector<std::string> tokens;
-
-            RemoveComments(line);
-            TrimString(line);
-            SplitString(line, ' ', tokens);
-
-            if (tokens.size() > 1) {
-                std::string value;
-
-                for (int i = 1; i < tokens.size(); i++) {
-                    value += tokens[i];
-                }
-
-                kv_pairs[tokens[0]] = value;
-            }
-        }
-
-        return true;
-    }
-
-    return false;
-}
-
-void RegSave(const char *file_name) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    for (auto &pair : kv_pairs) {
-        std::ofstream f;
-
-        f.open(file_name);
-        f << pair.first << " " << pair.second << "\n";
-    }
-}
-
-void RegSetString(const char *name, const char *value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    kv_pairs[name] = value;
-}
-
-void RegSetInt32(const char *name, int value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    kv_pairs[name] = std::to_string(value);
-}
-
-void RegSetFloat32(const char *name, float value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    kv_pairs[name] = std::to_string(value);
-}
-
-void RegSetFloat64(const char *name, double value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    kv_pairs[name] = std::to_string(value);
-}
-
-std::string RegGetString(const char *name, const char *default_value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end()) {
-        return value->second;
-    }
-    return default_value;
-}
-
-int RegGetInt32(const char *name, int default_value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end()) {
-        int ret = atoi(value->second.c_str());
-        return ret;
-    }
-    return default_value;
-}
-
-float RegGetFloat32(const char *name, float default_value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end()) {
-        float ret = std::stof(value->second.c_str());
-        return ret;
-    }
-    return default_value;
-}
-
-double RegGetFloat64(const char *name, double default_value) {
-    std::scoped_lock registry_lock(registry_mutex);
-
-    auto value = kv_pairs.find(name);
-    if (value != kv_pairs.end()) {
-        double ret = std::stod(value->second.c_str());
-        return ret;
-    }
-    return default_value;
-}
-
-#endif
-
+std::string          reg_get_string(const char *name, const char *default_value);
+int                  reg_get_i32(const char *name, int default_value);
+float                reg_get_f32(const char *name, float default_value);
+double               reg_get_f64(const char *name, double default_value);
+std::array<float, 3> reg_get_3f32(const char *name);
 #endif
